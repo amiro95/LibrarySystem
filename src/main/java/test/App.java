e4635de141e6a50b5f05afe063129915f1c8645d
DIR\amir.s.al.ghafar <amir.s.al.ghafar@accenture.com>
2020-07-30 21:55:15 +0800

    adding dependencies

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -2,6 +2,10 @@ package test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  * Hello world!
@@ -9,8 +13,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-public class App {
+@EnableEurekaClient
+@EnableFeignClients
+public class App extends SpringBootServletInitializer {
+
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(applicationClass, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<App> applicationClass = App.class;
 }
